13c32f275c1078468c01be1b2af34084f9bf8cfa
andrew sòlanto <atsolanto@gmail.com>
2025-03-16 12:45:06 -0400

    first commit

--- keyboards/nuphy/air75_v2/ansi/rules.mk
+++
@@ -1,3 +0,0 @@
-SRC += side.c rf.c sleep.c side_driver.c rf_driver.c
-UART_DRIVER_REQUIRED = yes
-
--- keyboards/nuphy/halo96_v2/ansi/keymaps/custom/keymap.c
+++ keyboards/nuphy/halo96_v2/ansi/keymaps/custom/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	_______,	_______,	_______,										_______, 							_______,	MO(1),   							RGB_MOD,	RGB_VAD,    RGB_HUI,	_______,	_______),
 // layer 2 win
 [2] = LAYOUT(
-	KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	MAC_PRTA,	KC_DEL,		KC_HOME,	KC_END,		KC_PGUP,	KC_PGDN,
+	KC_ESC, 	KC_F14,  	KC_F15,  	KC_F16, 	KC_WSCH,  	KC_F17,  	KC_F18,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	KC_PSCR,	KC_DEL,		KC_HOME,	KC_END,		KC_PGUP,	KC_PGDN,
 	KC_GRV, 	KC_1,   	KC_2,   	KC_3,  		KC_4,   	KC_5,   	KC_6,   	KC_7,   	KC_8,   	KC_9,  		KC_0,   	KC_MINS,	KC_EQL, 	KC_BSPC,				KC_NUM,		KC_PSLS,	KC_PAST,	KC_PMNS,
 	KC_TAB, 	KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSLS,				KC_P7,		KC_P8,		KC_P9,		KC_PPLS,
 	KC_CAPS,	KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	KC_ENT,								KC_P4,		KC_P5,		KC_P6,
@@ -28,11 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_LCTL,	KC_LGUI,	KC_LALT,										KC_SPC, 							KC_RALT,	MO(3),   							KC_LEFT,	KC_DOWN,    KC_RGHT,	KC_P0,		KC_PDOT),
 // layer 3 win Fn
 [3] = LAYOUT(
-	_______, 	KC_BRID,   	KC_BRIU,    _______,  	_______,   	_______,   	_______,   	KC_MPRV,   	KC_MPLY,   	KC_MNXT,  	KC_MUTE, 	KC_VOLD, 	KC_VOLU,	KC_PSCR,	KC_INS,		_______,	_______,	_______,	_______,
+	_______, 	KC_F1,   	KC_F2,      KC_F3,  	KC_F4,   	KC_F5,   	KC_F6,   	KC_F7,   	KC_F8,   	KC_F9,  	KC_F10, 	KC_F11, 	KC_F12,	    KC_F19,	    KC_INS,		_______,	_______,	_______,	_______,
 	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	_______, 	_______,				_______,	_______,	_______,	_______,
 	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	DEV_RESET,	SLEEP_MODE, BAT_SHOW,				_______,	_______,	_______,	_______,
 	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	_______,	_______,   	_______,   	_______,  	_______,	_______,  	_______,							_______,	_______,	_______,
-	_______,				_______,   	_______,   	_______,  	_______,   	_______,   	_______,	MO(4), 		RGB_SPD,	RGB_SPI,	_______, 	KC_NO,				    RGB_VAI,	_______,	_______,	_______,	_______,
+	_______,				_______,   	_______,   	_______,  	_______,   	_______,   	_______,	MO(4), 		RGB_SPD,	RGB_SPI,	_______, 	KC_NO,				    RGB_VAI,	_______,	_______,	_______,	RF_DFU,
 	_______,	_______,	_______,										_______, 							_______,	MO(3), 							    RGB_MOD,	RGB_VAD,    RGB_HUI,	_______,	_______),
 // layer 4 function
 [4] = LAYOUT(
